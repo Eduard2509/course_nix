1a7a54f6bf8e5afe22677403f368ee879f3fc488
Eduard_Miroshnichenko <edik.miroshnichenko@gmail.com>
2022-07-27 12:23:39 +0300

    lesson15

--- src/main/java/com/Main.java
+++ src/main/java/com/Main.java
@@ -2,16 +2,69 @@ package com;
 
 import com.command.Action;
 import com.command.Command;
+import com.model.Auto;
+import com.model.Manufacturer;
+import com.tree.Tree;
 import com.util.UserInputUtil;
 
+import java.math.BigDecimal;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 
 public class Main {
 
 
     public static void main(String[] args) {
 
+        Random random = new Random();
+
+        Auto auto = new Auto(
+                "Model-" + random.nextInt(1000),
+                Manufacturer.BMW,
+                BigDecimal.valueOf(random.nextDouble(1000.0)),
+                "Model-" + random.nextInt(1000), 1
+        );
+
+        Auto auto1 = new Auto(
+                "Model-" + random.nextInt(1000),
+                Manufacturer.KIA,
+                BigDecimal.valueOf(random.nextDouble(1000.0)),
+                "Model-" + random.nextInt(1000), 1
+        );
+
+        Auto auto2 = new Auto(
+                "Model-" + random.nextInt(1000),
+                Manufacturer.KIA,
+                BigDecimal.valueOf(random.nextDouble(1000.0)),
+                "Model-" + random.nextInt(1000), 1
+        );
+
+        Auto auto3 = new Auto(
+                "Model-" + random.nextInt(1000),
+                Manufacturer.BMW,
+                BigDecimal.valueOf(random.nextDouble(1000.0)),
+                "Model-" + random.nextInt(1000), 1
+        );
+
+        Auto auto4 = new Auto(
+                "Model-" + random.nextInt(1000),
+                Manufacturer.OPEL,
+                BigDecimal.valueOf(random.nextDouble(1000.0)),
+                "Model-" + random.nextInt(1000), 1
+        );
+
+        Tree tree = new Tree();
+        tree.add(auto1);
+        tree.add(auto2);
+        tree.add(auto3);
+        tree.add(auto4);
+        tree.printTree();
+        System.out.println(tree.sumLeftBranch());
+        System.out.println(tree.sumRightBranch());
+        System.out.println();
+        System.out.println("------------------- Tree End -------------------");
+        System.out.println();
 
         final Action[] actions = Action.values();
         final List<String> names = getNames(actions);
