0eb1d1b140f399f18ca98376d38440225fe4650c
Eduard_Miroshnichenko <edik.miroshnichenko@gmail.com>
2022-09-22 17:11:20 +0300

    servlet

--- src/main/java/com/hw31/src/main/java/com/servlets/MyServlet.java
+++ src/main/java/com/hw31/src/main/java/com/servlets/MyServlet.java
@@ -3,19 +3,17 @@ package com.servlets;
 import com.servlets.model.Visitor;
 
 import javax.servlet.ServletException;
-import javax.servlet.annotation.WebFilter;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.io.PrintWriter;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 
 @WebServlet(name = "MyServlet", value = "/MyServlet")
-public class MyServlet extends HttpServlet{
+public class MyServlet extends HttpServlet {
 
     private static List<Visitor> visitors = new ArrayList<>();
 
